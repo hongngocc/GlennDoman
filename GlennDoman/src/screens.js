36f2d0a08ba573bca98d528ad9f9a2fe700b3f5b
Phonganhung9xtb <windylovely93tb@gmail.com>
2018-02-11 00:38:02 +0700

    add edit lesson, tomorrow continue

--- GlennDoman/src/screens.js
+++ GlennDoman/src/screens.js
@@ -6,12 +6,14 @@ import Topic from './screens/topic/topic';
 import Settings from './screens/settings/settings';
 import AddLesson from './screens/lesson/add_lesson';
 import LessonDetail from './screens/lesson/lesson_detail';
+import EditLesson from './screens/lesson/edit_lesson';
 
 // register all screens of the app (including internal ones)
 export function registerScreens(store, Provider) {
     Navigation.registerComponent('kids.Lesson', () => Lesson, store, Provider);
     Navigation.registerComponent('kids.AddLesson', () => AddLesson, store, Provider);
     Navigation.registerComponent('kids.LessonDetail', () => LessonDetail, store, Provider);
+    Navigation.registerComponent('kids.EditLesson', () => EditLesson, store, Provider);
     Navigation.registerComponent('kids.History', () => History, store, Provider);
     Navigation.registerComponent('kids.Topic', () => Topic, store, Provider);
     Navigation.registerComponent('kids.Settings', () => Settings, store, Provider);
