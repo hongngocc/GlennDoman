0d4d9c37aa28cf4f75dfdd318430788c62085f99
Quang Huy <myheaven291@gmail.com>
2018-02-10 11:30:47 +0700

    add lib realm

--- GlennDoman/android/app/src/main/java/com/glenndoman/MainApplication.java
+++ GlennDoman/android/app/src/main/java/com/glenndoman/MainApplication.java
@@ -9,54 +9,21 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import io.realm.react.RealmReactPackage;
+
 public class MainApplication extends NavigationApplication {
-
-  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-  //   @Override
-  //   public boolean getUseDeveloperSupport() {
-  //     return BuildConfig.DEBUG;
-  //   }
-
-  //   @Override
-  //   protected List<ReactPackage> getPackages() {
-  //     return Arrays.<ReactPackage>asList(
-  //         new MainReactPackage(),
-            // new VectorIconsPackage(),
-  //           new NavigationReactPackage()
-  //     );
-  //   }
-
-  //   @Override
-  //   protected String getJSMainModuleName() {
-  //     return "index";
-  //   }
-  // };
-
-  // @Override
-  // public ReactNativeHost getReactNativeHost() {
-  //   return mReactNativeHost;
-  // }
-
-  // @Override
-  // public void onCreate() {
-  //   super.onCreate();
-  //   SoLoader.init(this, /* native exopackage */ false);
-  // }
 
        @Override
      public boolean isDebug() {
-         // Make sure you are using BuildConfig from your own application
          return BuildConfig.DEBUG;
      }
 
      protected List<ReactPackage> getPackages() {
-         // Add additional packages you require here
-         // No need to add RnnPackage and MainReactPackage
          return Arrays.<ReactPackage>asList(
-                          new MainReactPackage(),
-                            new NavigationReactPackage(),
-                            new VectorIconsPackage()
-             // eg. new VectorIconsPackage()
+                new MainReactPackage(),
+                new NavigationReactPackage(),
+                new VectorIconsPackage(),
+                new RealmReactPackage()
          );
      }
 
